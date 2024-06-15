bff0e69f70aea1480cb067854bf3af36b9bfac65
Kutlu <kutlu.karakoc@akinon.com>
2024-06-15 17:23:09 +0300

    feat: completed charts screen ui

--- app/(tabs)/charts/index.tsx
+++ app/(tabs)/charts/index.tsx
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
+import { Text, ScrollView } from "react-native";
+import { StatisticsByCategory } from "~/components/StatisticsByCategory";
+import { StatisticsByDate } from "~/components/StatisticsByDate";
 
 export default function ChartsScreen() {
   return (
-    <View>
-      <Text>
-        Charts
+    <ScrollView className="flex-1 px-4" showsVerticalScrollIndicator={false}>
+      <Text className="text-foreground text-lg native:text-lg font-medium mb-9">
+        Harcama Analizi
       </Text>
-    </View>
+
+      <StatisticsByDate />
+      <StatisticsByCategory />
+    </ScrollView>
   );
 }
