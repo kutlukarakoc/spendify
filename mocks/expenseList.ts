f283cb52efb5e0ddd4f11b43109e9d182235bc9e
Kutlu <kutlu.karakoc@akinon.com>
2024-06-19 01:00:32 +0300

    feat: added new keys to expenseList mock data

--- mocks/expenseList.ts
+++ mocks/expenseList.ts
@@ -1,50 +1,66 @@
 export const expenseList = [
   {
-    category: "transport",
+    spendTo: "Uber",
     description: "Taksi Ödemesi",
+    category: "transport",
     amount: 250,
     date: "1716461319",
+    note: "Havaalanına gitmek için"
   },
   {
+    spendTo: "Zara",
     category: "shopping",
     description: "Hediye",
     amount: 675,
     date: "1716461319",
+    note: "Eşimin doğum günü için"
   },
   {
+    spendTo: "Udemy",
     category: "education",
     description: "Kurs Ödemesi",
     amount: 2500,
     date: "1716461319",
+    note: "React Native kursu için"
   },
   {
+    spendTo: "Turkcell",
     category: "invoice",
     description: "Telefon Faturası",
     amount: 300,
     date: "1716461319",
+    note: "Ağustos ayı faturası"
   },
   {
+    spendTo: "Cinema",
     category: "entertainment",
     description: "Sinema Bileti",
     amount: 200,
     date: "1716461319",
+    note: "Dune filmi için"
   },
   {
+    spendTo: "Dr. John Doe",
     category: "health",
     description: "Doktor Muayenesi",
     amount: 1000,
     date: "1716461319",
+    note: "Boyun ağrılarım için"
   },
   {
+    spendTo: "McDonalds",
     category: "food",
     description: "Öğle Yemeği",
     amount: 300,
     date: "1716461319",
+    note: "Hamburger menü"
   },
   {
+    spendTo: "Amazon",
     category: "other",
     description: "Diğer",
     amount: 100,
     date: "1716461319",
+    note: "Kitap alışverişi"
   }
 ];
