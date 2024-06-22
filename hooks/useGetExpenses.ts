97c2573a2e36f1084d1d0665ab27454c31a8ad8e
Kutlu <kutlu.karakoc@akinon.com>
2024-06-22 15:18:56 +0300

    feat: Added useGetExpenses hook for fetching and filtering expenses

---
+++ hooks/useGetExpenses.ts
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { TransformedCategory } from "~/lib/helpers/transformCategoriesToArr";
+import { getStorageItem } from "~/lib/utils";
+import { Expense } from "~/types/expense";
+import { GetStorageItem } from "~/types/getStorageItemReturn";
+
+export const useGetExpenses = () => {
+  const [expenseList, setExpenseList] = useState<Expense[] | null>(null);
+  const [expensesError, setExpensesError] = useState<string | null>(null);
+  const [isExpensesFetching, setIsExpensesFetching] = useState(false);
+
+  const getExpenses = async (
+    searchQuery: string,
+    selectedCategory: TransformedCategory | null
+  ) => {
+    setIsExpensesFetching(true);
+
+    const expenses: GetStorageItem = await getStorageItem("expenses");
+
+    if (expenses.status === "error") {
+      if (expenseList !== null) {
+        setExpenseList(null);
+      }
+      setExpensesError("Error fetching expenses");
+      setIsExpensesFetching(false);
+
+      return;
+    }
+
+    if (expenses.data === null) {
+      if (expenseList !== null) {
+        setExpenseList(null);
+      }
+      setIsExpensesFetching(false);
+
+      return;
+    }
+
+    const filteredExpenses = filterExpenses(
+      expenses.data,
+      searchQuery,
+      selectedCategory
+    );
+
+    setExpenseList(filteredExpenses);
+    setIsExpensesFetching(false);
+    if (expensesError !== null) {
+      setExpensesError(null);
+    }
+  };
+
+  const filterExpenses = (
+    expenses: Expense[],
+    searchQuery: string,
+    selectedCategory: TransformedCategory | null
+  ) => {
+    let filteredExpenses = expenses;
+
+    if (searchQuery.length > 0) {
+      filteredExpenses = filteredExpenses.filter((expense: Expense) =>
+        expense.description.includes(searchQuery)
+      );
+    }
+
+    if (selectedCategory !== null) {
+      filteredExpenses = filteredExpenses.filter(
+        (expense: Expense) => expense.category === selectedCategory.id
+      );
+    }
+
+    return filteredExpenses;
+  };
+
+  const cleanupExpenseStates = () => {
+    setExpenseList(null);
+    setExpensesError(null);
+    setIsExpensesFetching(false);
+  };
+
+  return {
+    getExpenses,
+    cleanupExpenseStates,
+    expenseList,
+    expensesError,
+    isExpensesFetching,
+  };
+};
