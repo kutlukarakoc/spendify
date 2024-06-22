cd5ccf24e69669fa5c2e6b6410a4a40e516d36e5
Kutlu <kutlu.karakoc@akinon.com>
2024-06-22 15:14:34 +0300

    fix: removed formatTimestamp function

--- lib/helpers/formatTimestamp.ts
+++
@@ -1,8 +0,0 @@
-export const formatTimestamp = (timestampString: string): string => {
-  const timestamp = new Date(parseInt(timestampString) * 1000);
-  const day = ("0" + timestamp.getDate()).slice(-2);
-  const month = ("0" + (timestamp.getMonth() + 1)).slice(-2);
-  const year = timestamp.getFullYear().toString().slice(-2);
-
-  return `${day}/${month}/${year}`;
-};
