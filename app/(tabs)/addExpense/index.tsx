d08e27e62acfd86c6362ef5d4dff3295226e2a93
Kutlu <kutlu.karakoc@akinon.com>
2024-06-22 15:18:27 +0300

    feat: added add expense to async storage logic

--- app/(tabs)/addExpense/index.tsx
+++ app/(tabs)/addExpense/index.tsx
@@ -1,24 +1,85 @@
 import { useState } from "react";
+import { Alert, ScrollView, View } from "react-native";
 import DateTimePicker, { DateType } from "react-native-ui-datepicker";
-import { ScrollView, View } from "react-native";
+import { Loading } from "./Loading";
+import { CategoryRadioItem } from "./CategoryRadioItem";
+import { ErrorText } from "./ErrorText";
+import { RequiredMark } from "./RequiredMark";
 import { Text } from "~/components/ui/text";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { RadioGroup } from "~/components/ui/radio-group";
+import { getStorageItem, setStorageItem } from "~/lib/utils";
 import { transformCategoriesToArray } from "~/lib/helpers/transformCategoriesToArr";
-import "dayjs/locale/tr";
 import { useColorScheme } from "~/lib/useColorScheme";
+import { addExpenseSchema } from "~/lib/schemas/addExpenseSchema";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import "dayjs/locale/tr";
+import dayjs from "dayjs";
+
+type AddExpenseForm = {
+  spendTo: string;
+  description: string;
+  category: string;
+  amount: string;
+  date: DateType;
+  note: string;
+};
 
 export default function AddExpenseScreen() {
   const { isDarkColorScheme } = useColorScheme();
-
-  const [category, setCategory] = useState("");
-  const [date, setDate] = useState<DateType | undefined>();
-
   const calenderTextsColor = isDarkColorScheme
     ? { color: "#fff" }
     : { color: "#000" };
+
+  const { control, reset, handleSubmit } = useForm({
+    defaultValues: {
+      spendTo: "",
+      description: "",
+      category: "other",
+      amount: "",
+      date: dayjs(),
+      note: "",
+    },
+    resolver: zodResolver(addExpenseSchema),
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = async (data: AddExpenseForm) => {
+    setIsSubmitting(true);
+
+    const expenses = await getStorageItem("expenses");
+
+    if (expenses.status === "error") {
+      setIsSubmitting(false);
+      Alert.alert("Bir hata oluştu. Lütfen tekrar deneyin.");
+      return;
+    }
+
+    const expensesArr = expenses.data !== null ? expenses.data : [];
+    const newExpense = {
+      ...data,
+      date: dayjs(data.date).unix(),
+      amount: Number(data.amount),
+      id: Date.now(),
+    };
+    const updatedExpenses = [newExpense, ...expensesArr];
+
+    const result = await setStorageItem("expenses", updatedExpenses);
+    if (result === "error") {
+      setIsSubmitting(false);
+      Alert.alert("Bir hata oluştu. Lütfen tekrar deneyin.");
+      return;
+    }
+
+    setIsSubmitting(false);
+    Alert.alert("Gider başarıyla eklendi.");
+
+    reset();
+  };
 
   return (
     <ScrollView className="flex-1 px-4" showsVerticalScrollIndicator={false}>
@@ -26,130 +87,167 @@ export default function AddExpenseScreen() {
         Gider Kaydı
       </Text>
 
-      <View className="flex-col gap-y-5 mt-9">
-        <View className="gap-y-3">
-          <Label
-            className="text-foreground font-normal"
-            nativeID="seller-buyer"
-          >
+      <View className="flex-col gap-y-7 mt-9">
+        <View className="gap-y-2">
+          <Label className="text-foreground font-normal" nativeID="spendTo">
             Alıcı/Satıcı
           </Label>
-          <Input
-            className="native:h-12 px-4 border-2 text-sm native:text-sm"
-            placeholder="Alıcı veya satıcı ismi giriniz"
-            aria-labelledby="seller-buyer"
-          />
-        </View>
-
-        <View className="gap-y-3">
-          <Label
-            className="text-foreground font-normal"
-            nativeID="expense-name"
-          >
-            Gider Adı
-          </Label>
-          <Input
-            className="native:h-12 px-4 border-2 text-sm native:text-sm"
-            placeholder="Gider adı giriniz"
-            aria-labelledby="expense-name"
-          />
-        </View>
-
-        <View className="gap-y-3 px-1">
-          <Label nativeID="category" className="text-foreground font-normal">
-            Kategori
-          </Label>
-          <RadioGroup
-            value={category}
-            onValueChange={setCategory}
-            className="flex-wrap flex-row gap-3"
-          >
-            {transformCategoriesToArray().map((category) => (
-              <CategoryRadioItem
-                key={category.id}
-                value={category.id}
-                label={category.name}
-                onLabelPress={() => setCategory(category.id)}
+          <Controller
+            control={control}
+            name="spendTo"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <Input
+                className="native:h-12 px-4 border-2 text-sm native:text-sm"
+                placeholder="Alıcı veya satıcı ismi giriniz"
+                aria-labelledby="spendTo"
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
               />
-            ))}
-          </RadioGroup>
-        </View>
-
-        <View className="gap-y-3">
-          <Label
-            className="text-foreground font-normal"
-            nativeID="expense-amount"
-          >
-            Gider Tutarı
-          </Label>
-          <Input
-            className="native:h-12 px-4 border-2 text-sm native:text-sm"
-            placeholder="Gider tutarı giriniz"
-            aria-labelledby="expense-amount"
-            keyboardType="numeric"
+            )}
           />
         </View>
 
-        <View className="gap-y-3">
+        <View className="gap-y-2">
+          <Label className="text-foreground font-normal" nativeID="description">
+            Gider Adı <RequiredMark />
+          </Label>
+          <Controller
+            control={control}
+            name="description"
+            render={({
+              field: { value, onChange, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  className="native:h-12 px-4 border-2 text-sm native:text-sm"
+                  placeholder="Gider adı giriniz"
+                  aria-labelledby="description"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                />
+                {error?.message && <ErrorText message={error.message} />}
+              </>
+            )}
+          />
+        </View>
+
+        <View className="gap-y-2 px-1">
+          <Label nativeID="category" className="text-foreground font-normal">
+            Kategori <RequiredMark />
+          </Label>
+          <Controller
+            control={control}
+            name="category"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <>
+                <RadioGroup
+                  value={value}
+                  onValueChange={onChange}
+                  className="flex-wrap flex-row gap-3"
+                >
+                  {transformCategoriesToArray().map((category) => (
+                    <CategoryRadioItem
+                      key={category.id}
+                      value={category.id}
+                      label={category.name}
+                      onPress={() => onChange(category.id)}
+                    />
+                  ))}
+                </RadioGroup>
+                {error?.message && <ErrorText message={error.message} />}
+              </>
+            )}
+          />
+        </View>
+
+        <View className="gap-y-2">
+          <Label className="text-foreground font-normal" nativeID="amount">
+            Gider Tutarı <RequiredMark />
+          </Label>
+          <Controller
+            control={control}
+            name="amount"
+            render={({
+              field: { value, onChange, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  className="native:h-12 px-4 border-2 text-sm native:text-sm"
+                  placeholder="Gider tutarı giriniz"
+                  aria-labelledby="amount"
+                  keyboardType="numeric"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                />
+                {error?.message && <ErrorText message={error.message} />}
+              </>
+            )}
+          />
+        </View>
+
+        <View className="gap-y-2">
           <Label
             className="text-foreground font-normal"
             nativeID="expense-date"
           >
-            Tarih
+            Tarih <RequiredMark />
           </Label>
-          <DateTimePicker
-            mode="single"
-            locale="tr"
-            onChange={(params) => setDate(params.date)}
-            date={date}
-            headerButtonColor={isDarkColorScheme ? "#fff" : "#000"}
-            headerTextStyle={calenderTextsColor}
-            todayTextStyle={calenderTextsColor}
-            weekDaysTextStyle={calenderTextsColor}
-            calendarTextStyle={calenderTextsColor}
+          <Controller
+            control={control}
+            name="date"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <>
+                <DateTimePicker
+                  mode="single"
+                  locale="tr"
+                  onChange={({ date }) => onChange(date)}
+                  date={value}
+                  headerButtonColor={isDarkColorScheme ? "#fff" : "#000"}
+                  headerTextStyle={calenderTextsColor}
+                  todayTextStyle={calenderTextsColor}
+                  weekDaysTextStyle={calenderTextsColor}
+                  calendarTextStyle={calenderTextsColor}
+                />
+                {error?.message && <ErrorText message={error.message} />}
+              </>
+            )}
           />
         </View>
 
-        <View className="gap-y-3">
-          <Label
-            className="text-foreground font-normal"
-            nativeID="expense-note"
-          >
+        <View className="gap-y-2">
+          <Label className="text-foreground font-normal" nativeID="note">
             Not
           </Label>
-          <Input
-            className="native:h-36 px-4 border-2 text-sm native:text-sm"
-            multiline
-            placeholder="Giderle ilgili bir not ekleyin"
-            aria-labelledby="expense-note"
-            numberOfLines={8}
-            style={{ textAlignVertical: "top" }}
+          <Controller
+            control={control}
+            name="note"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <Input
+                className="native:h-36 px-4 border-2 text-sm native:text-sm"
+                multiline
+                placeholder="Giderle ilgili bir not ekleyin"
+                aria-labelledby="note"
+                numberOfLines={8}
+                style={{ textAlignVertical: "top" }}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+              />
+            )}
           />
         </View>
       </View>
 
-      <Button className="mt-9 mb-14">
+      <Button className="mt-9 mb-14" onPress={handleSubmit(onSubmit)}>
         <Text className="text-white">Kaydet</Text>
       </Button>
-    </ScrollView>
-  );
-}
 
-function CategoryRadioItem({
-  value,
-  onLabelPress,
-  label,
-}: {
-  value: string;
-  onLabelPress: () => void;
-  label: string;
-}) {
-  return (
-    <View className={"flex-row gap-2 items-center min-w-28"}>
-      <RadioGroupItem aria-labelledby={`label-for-${value}`} value={value} />
-      <Label nativeID={`label-for-${value}`} onPress={onLabelPress}>
-        {label}
-      </Label>
-    </View>
+      {isSubmitting && <Loading />}
+    </ScrollView>
   );
 }
