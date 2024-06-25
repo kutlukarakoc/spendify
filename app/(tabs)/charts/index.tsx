fe1d22c618c1d13aec8758dce8ca1c38a359685f
Kutlu <kutlu.karakoc@akinon.com>
2024-06-25 23:55:16 +0300

    refactor: Update ChartsScreen component to use useGetAllExpenses hook

--- app/(tabs)/charts/index.tsx
+++ app/(tabs)/charts/index.tsx
@@ -1,8 +1,42 @@
+import { useEffect } from "react";
 import { Text, ScrollView } from "react-native";
 import { StatisticsByCategory } from "~/components/StatisticsByCategory";
 import { StatisticsByDate } from "~/components/StatisticsByDate";
+import { useIsFocused } from "@react-navigation/native";
+import { useGetAllExpenses } from "~/hooks/useGetAllExpenses";
 
 export default function ChartsScreen() {
+  const isFocused = useIsFocused();
+
+  const {
+    getExpenses,
+    cleanupExpenseStates,
+    expenseList,
+    totalExpenseCount,
+    expensesError,
+    isExpensesFetching,
+  } = useGetAllExpenses();
+
+  useEffect(() => {
+    if (isFocused) {
+      getExpenses();
+    } else {
+      cleanupExpenseStates();
+    }
+  }, [isFocused]);
+
+  if (isExpensesFetching) {
+    return <Text>loading..</Text>;
+  }
+
+  if (expensesError) {
+    return <Text>Hata</Text>;
+  }
+
+  if (expenseList === null) {
+    return <Text>Veri yok</Text>;
+  }
+
   return (
     <ScrollView className="flex-1 px-4" showsVerticalScrollIndicator={false}>
       <Text className="text-foreground text-lg native:text-lg font-medium mb-9">
@@ -10,7 +44,7 @@ export default function ChartsScreen() {
       </Text>
 
       <StatisticsByDate />
-      <StatisticsByCategory />
+      <StatisticsByCategory expenseList={expenseList} />
     </ScrollView>
   );
 }
